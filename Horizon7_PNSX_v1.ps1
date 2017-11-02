49774ba591c781d3fb5fb85e5a295174d446b265
vwilmo <33289176+vwilmo@users.noreply.github.com>
2017-11-02 15:05:50 -0500

    Add files via upload

--- Horizon7_PNSX_v1.ps1
+++ Horizon7_PNSX_v1.ps1
@@ -15,32 +15,77 @@
 
     )
 
-    #Build New Horizon 7 DFW Sections
-    $SNHorizonDesktopBlockSectionName = "Horizon Desktop Block Section"
-    $SNHorizonDesktopBlockSection = Get-NsxFirewallSection -Name $SNHorizonDesktopBlockSectionName
-    if (!$SNHorizonDesktopBlockSection)
+    ##Build New Horizon 7 DFW Sections
+    ##Checks if Section exists first
+    
+    $SNHZN7ConnExternalSectionName = "Horizon 7 Connectivity - External Connections"
+    $SNHZN7ConnExternalSection = Get-NsxFirewallSection -Name $SNHZN7ConnExternalSectionName
+    if (!$SNHZN7ConnExternalSection)
     {
-      $SNHorizonDesktopBlockSection = New-NsxFirewallSection $SNHorizonDesktopBlockSectionName
+      $SNHZN7ConnExternalSection = New-NsxFirewallSection $SNHZN7ConnExternalSectionName 
     }
-    $SNHorizonViewManagementBlockSectionName = "Horizon View Management Block Section"
-    $SNHorizonViewManagementBlockSection = Get-NsxFirewallSection -Name $SNHorizonViewManagementBlockSectionName
-    if (!$SNHorizonViewManagementBlockSection)
+    $SNHZN7ConnTunneledSectionName = "Horizon 7 Connectivity - Tunneled Connections"
+    $SNHZN7ConnTunneledSection = Get-NsxFirewallSection -Name $SNHZN7ConnTunneledSectionName
+    if (!$SNHZN7ConnTunneledSectionName)
     {
-      $SNHorizonViewManagementBlockSection = New-NsxFirewallSection $SNHorizonViewManagementBlockSectionName
+      $SNHZN7ConnTunneledSection = New-NsxFirewallSection $SNHZN7ConnTunneledSectionName -position bottom
     }
-    $SNHorizonViewExternalConnectionsSectionName = "Horizon View External Connections Section"
-    $SNHorizonViewExternalConnectionsSection = Get-NsxFirewallSection -Name $SNHorizonViewExternalConnectionsSectionName
-    if (!$SNHorizonViewExternalConnectionsSection)
+    $SNHZN7ConnInternalSectionName = "Horizon 7 Connectivity - Internal Connections"
+    $SNHZN7ConnInternalSection = Get-NsxFirewallSection -Name $SNHZN7ConnInternalSectionName
+    if (!$SNHZN7ConnInternalSection)
     {
-      $SNHorizonViewExternalConnectionsSection = New-NsxFirewallSection $SNHorizonViewExternalConnectionsSectionName
+      $SNHZN7ConnInternalSection = New-NsxFirewallSection $SNHZN7ConnInternalSectionName -position bottom
     }
-    $SNHorizonOperationManagementSectionName = "Horizon Operation Management Section"
-    $SNHorizonOperationManagementSection = Get-NsxFirewallSection -Name $SNHorizonOperationManagementSectionName
-    if (!$SNHorizonOperationManagementSection)
+    $SNHZN7DesktopVDI_RDSHSectionName = "Horizon 7 Desktops - VDI or RDS Host"
+    $SNHZN7DesktopVDI_RDSHSection = Get-NsxFirewallSection -Name $SNHZN7DesktopVDI_RDSHSectionName
+    if (!$SNHZN7DesktopVDI_RDSHSection)
     {
-      $SNHorizonOperationManagementSection = New-NsxFirewallSection $SNHorizonOperationManagementSectionName
+      $SNHZN7DesktopVDI_RDSHSection = New-NsxFirewallSection $SNHZN7DesktopVDI_RDSHSectionName -position bottom
     }
-    #Build New Security Tags
+    $SNHZN7InfraConnServerSectionName = "Horizon 7 Infrastructure - View Connection Server"
+    $SNHZN7InfraConnServerSection = Get-NsxFirewallSection -Name $SNHZN7InfraConnServerSectionName
+    if (!$SNHZN7InfraConnServerSection)
+    {
+      $SNHZN7InfraConnServerSection = New-NsxFirewallSection $SNHZN7InfraConnServerSectionName -position bottom
+    }
+    $SNHZN7InfravCenterViewCompSectionName = "Horizon 7 Infrastructure - vCenter Server and View Composer"
+    $SNHZN7InfravCenterViewCompSection = Get-NsxFirewallSection -Name $SNHZN7InfravCenterViewCompSectionName
+    if (!$SNHZN7InfravCenterViewCompSection)
+    {
+      $SNHZN7InfravCenterViewCompSection = New-NsxFirewallSection $SNHZN7InfravCenterViewCompSectionName -position bottom
+    }
+    $SNHZN7InfraUAGSectionName = "Horizon 7 Infrastructure - Unified Access Gateway"
+    $SNHZN7InfraUAGSection = Get-NsxFirewallSection -Name $SNHZN7InfraUAGSectionName
+    if (!$SNHZN7InfraUAGSection)
+    {
+      $SNHZN7InfraUAGSection = New-NsxFirewallSection $SNHZN7InfravUAGSectionName -position bottom
+    }
+    $SNHZN7InfravIDMSectionName = "Horizon 7 Infrastructure - VMware Identity Manager"
+    $SNHZN7InfravIDMSection = Get-NsxFirewallSection -Name $SNHZN7InfravIDMSectionName
+    if (!$SNHZN7InfravIDMSection)
+    {
+      $SNHZN7InfravIDMSectionn = New-NsxFirewallSection $SNHZN7InfravIDMSectionName -position bottom
+    }
+    $SNHZN7InfraAppVolMgrSectionName = "Horizon 7 Infrastructure - App Volumes Manager"
+    $SNHZN7InfraAppVolMgrSection = Get-NsxFirewallSection -Name $SNHZN7InfraAppVolMgrSectionnName
+    if (!$SNHZN7InfraAppVolMgrSection)
+    {
+      $SNHZN7InfraAppVolMgrSection = New-NsxFirewallSection $SNHZN7InfraAppVolMgrSectionName -position bottom
+    }
+    $SNHZN7InfraV4HSectionName = "Horizon 7 Infrastructure - vRealize Operations for Horizon"
+    $SNHZN7InfraV4HSection = Get-NsxFirewallSection -Name $SNHZN7InfraV4HSectionName
+    if (!$SNHZN7InfraV4HSection)
+    {
+      $SNHZN7InfraV4HSection = New-NsxFirewallSection $SNHZN7InfraV4HSectionName -position bottom
+    }
+    $SNHHZN7InfraMgmtSectionName = "Horizon 7 Infrastructure - Management"
+    $SNHZN7InfraMgmtSection = Get-NsxFirewallSection -Name $SNHZN7InfraMgmtSectionName
+    if (!$SNHZN7InfraMgmtSection)
+    {
+      $SNHZN7InfraMgmtSection = New-NsxFirewallSection $SNHZN7InfraMgmtSectionName -position bottom
+    }
+    ##Build New Security Tags
+    ##Checks if exists first
     $STHorizonViewDesktopsName = "ST-Horizon 7 View Desktops"
     $STHorizonViewDesktops = Get-NsxSecurityTag -Name $STHorizonViewDesktopsName
     if (!$STHorizonViewDesktops)
