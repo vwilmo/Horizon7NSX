b975527c9ec4e63649a7a211dca975233883d75f
vwilmo <33289176+vwilmo@users.noreply.github.com>
2017-11-03 15:11:19 -0500

    Add files via upload

--- Horizon7_PNSX_v1.ps1
+++ Horizon7_PNSX_v1.ps1
@@ -3,14 +3,14 @@
         ##Creating Horizon 7 Groupings
         
         #Security Groups
-        $SGHZN7ClientName = "SG-Horizon7Client",
-        $SGHZN7ConnServerName = "SG-Horizon7ConnServer",
-        $SGHZN7vIDMName = "SG-Horizon7vIDM",
-        $SGHZN7UAGName = "SG-Horizon7UAG",
-        $SGHZN7VDIName = "SG-Horizon7VDI",
-        $SGHZN7RDSHoSGName = "SG-Horizon7RDSHoSG",
-        $SGHZN7EnrollServerName = "SG-Horizon7EnrollServer",
-        $SGHZN7AppVolMgrName = "SG-Horizon7AppVolMgr",
+        $SGHZN7ClientName = "SG-Horizon7-Client",
+        $SGHZN7ConnServerName = "SG-Horizon7-ConnServer",
+        $SGHZN7vIDMName = "SG-Horizon7-vIDM",
+        $SGHZN7UAGName = "SG-Horizon7-UAG",
+        $SGHZN7VDIName = "SG-Horizon7-VDI",
+        $SGHZN7RDSHostName = "SG-Horizon7-RDSHost",
+        $SGHZN7EnrollServerName = "SG-Horizon7-EnrollServer",
+        $SGHZN7AppVolMgrName = "SG-Horizon7-AppVolMgr",
         $SGHZNvCenterMGMTName = "SG-Horizon7-vCenterMgmt",
         $SGHZNvCenterVDI_RDSHName = "SG-Horizon7-vCenterVDI_RDSH",
         $SGHZNUEM_FSName = "SG-Horizon7-UEM_FS",
@@ -98,14 +98,14 @@
     ##Build New Security Tags
     ##Checks if exists first
     $SecurityTagNames = 
-    "ST-Horizon7Client",
-    "ST-Horizon7ConnServer",
-    "ST-Horizon7vIDM",
-    "ST-Horizon7UAG",
-    "ST-Horizon7VDI",
-    "ST-Horizon7RDSHost",
-    "ST-Horizon7EnrollServer",
-    "ST-Horizon7AppVolMgr",
+    "ST-Horizon7-Client",
+    "ST-Horizon7-ConnServer",
+    "ST-Horizon7-vIDM",
+    "ST-Horizon7-UAG",
+    "ST-Horizon7-VDI",
+    "ST-Horizon7-RDSHost",
+    "ST-Horizon7-EnrollServer",
+    "ST-Horizon7-AppVolMgr",
     "ST-Horizon7-vCenterMgmt",
     "ST-Horizon7-vCenterVDI_RDSH",
     "ST-Horizon7-UEM_FS",
@@ -120,15 +120,63 @@
     "ST-Horizon7-AirWatch",
     "ST-Horizon7-V4H"
     foreach ($item in $SecurityTagNames) {
-    $SecurityTag = Get-NsxFirewallSection -Name "$item"
+    $SecurityTag = Get-NsxSecurityTag -Name "$item"
     if (!$SecurityTag)
     {
     New-NsxSecurityTag -name $item | out-null
     }}
 
     #Build New Security Groups
-    
-        #Build New Services
+    #Section for Future nested loop to attach ST to SG
+    #$SecurityGroupNames = 
+    #"SG-Horizon7Client",
+    #"SG-Horizon7ConnServer",
+    #"SG-Horizon7vIDM",
+    #"SG-Horizon7UAG",
+    #"SG-Horizon7VDI",
+    #"SG-Horizon7RDSHoSG",
+    #"SG-Horizon7EnrollServer",
+    #"SG-Horizon7AppVolMgr",
+    #"SG-Horizon7-vCenterMgmt",
+    #"SG-Horizon7-vCenterVDI_RDSH",
+    #"SG-Horizon7-UEM_FS",
+    #"SG-Horizon7-UEM_Mgr",
+    #"SG-Horizon7-Composer",
+    #"SG-Horizon7-DomainCtrl",
+    #"SG-Horizon7-DNS",
+    #"SG-Horizon7-SMTP",
+    #"SG-Horizon7-Citrix",
+    #"SG-Horizon7-ThinApp_FS",
+    #"SG-Horizon7-2FA",
+    #"SG-Horizon7-AirWatch",
+    #"SG-Horizon7-V4H"
+    #foreach ($item in $SecurityGroupNames) {
+    #$SecurityGroup = Get-NsxSecurityGroup -Name "$item"
+    #if (!$SecurityGroup)
+    #{
+    #New-NsxSecurityGroup -name $item -IncludeMember   out-null
+    #}}
+    $SGHZN7Client = New-NsxSecurityGroup -name $SGHZN7ClientName -IncludeMember (Get-NsxSecurityTag -Name $STHZN7ClientName)
+    $SGHZN7ConnServer = New-NsxSecurityGroup -name $SGHZN7ConnServerName -IncludeMember (Get-NsxSecurityTag -Name $STHZN7ConnServerName)
+    $SGHZN7vIDM = New-NsxSecurityGroup -name $SGHZN7vIDMName -IncludeMember (Get-NsxSecurityTag -Name $STHZN7vIDMName)
+    $SGHZN7UAG = New-NsxSecurityGroup -name $SGHZN7UAGName -IncludeMember (Get-NsxSecurityTag -Name $STHZN7UAGName)
+    $SGHZN7VDI = New-NsxSecurityGroup -name $SGHZN7VDIName -IncludeMember (Get-NsxSecurityTag -Name $STHZN7VDIName)
+    $SGHZN7RDSHost = New-NsxSecurityGroup -name $SGHZN7RDSHostName -IncludeMember (Get-NsxSecurityTag -Name $STHZN7RDSHostName)
+    $SGHZN7EnrollServer = New-NsxSecurityGroup -name $SGHZN7EnrollServerName -IncludeMember (Get-NsxSecurityTag -Name $STHZN7EnrollServerName)
+    $SGHZN7AppVolMgr = New-NsxSecurityGroup -name $SGHZN7AppVolMgrName -IncludeMember (Get-NsxSecurityTag -Name $STHZN7AppVolMgrName)
+    $SGHZNvCenterMGMT = New-NsxSecurityGroup -name $SGHZN7ClientName -IncludeMember (Get-NsxSecurityTag -Name $STHZN7ClientName)
+    $SGHZNvCenterVDI_RDSH = New-NsxSecurityGroup -name $SGHZN7vCenterVDI_RDSHName -IncludeMember (Get-NsxSecurityTag -Name $STHZN7vCenterVDI_RDSHName)
+    $SGHZNUEM_FS = New-NsxSecurityGroup -name $SGHZN7UEM_FSName -IncludeMember (Get-NsxSecurityTag -Name $STHZN7UEM_FSName)
+    $SGHZNUEM_Mgr = New-NsxSecurityGroup -name $SGHZN7UEM_MgrName -IncludeMember (Get-NsxSecurityTag -Name $STHZN7UEM_MgrName)
+    $SGHZNComposer = New-NsxSecurityGroup -name $SGHZN7ComposerName -IncludeMember (Get-NsxSecurityTag -Name $STHZN7ComposerName)
+    $SGHZNDomainCtrl = New-NsxSecurityGroup -name $SGHZN7DomainCtrlName -IncludeMember (Get-NsxSecurityTag -Name $STHZN7DomainCtrlName)
+    $SGHZNDNS = New-NsxSecurityGroup -name $SGHZN7DNSName -IncludeMember (Get-NsxSecurityTag -Name $STHZN7DNSName)
+    $SGHZNSMTP = New-NsxSecurityGroup -name $SGHZN7SMTPName -IncludeMember (Get-NsxSecurityTag -Name $STHZN7SMTPName)
+    $SGHZNCitrix = New-NsxSecurityGroup -name $SGHZN7CitrixName -IncludeMember (Get-NsxSecurityTag -Name $STHZN7CitrixName)
+    $SGHZNThinApp_FS = New-NsxSecurityGroup -name $SGHZN7ThinApp_FSName -IncludeMember (Get-NsxSecurityTag -Name $STHZN7ThinApp_FSName)
+    $SGHZN2FA = New-NsxSecurityGroup -name $SGHZN72FAName -IncludeMember (Get-NsxSecurityTag -Name $STHZN72FAName)
+    $SGHZNAirWatch = New-NsxSecurityGroup -name $SGHZN7AirWatchName -IncludeMember (Get-NsxSecurityTag -Name $STHZN7AirWatchName)
+    #Build New Services
     
     #Build New Service Groups
    
